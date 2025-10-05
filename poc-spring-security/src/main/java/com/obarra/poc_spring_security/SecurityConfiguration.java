d460eca45a65150727026ad53175f882be40151e
obarra-dev <obarra@sonatype.com>
2025-10-05 02:38:20 -0300

    enhance security configuration with password encoding and additional users

--- poc-spring-security/src/main/java/com/obarra/poc_spring_security/SecurityConfiguration.java
+++ poc-spring-security/src/main/java/com/obarra/poc_spring_security/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,8 +27,24 @@ public class SecurityConfiguration
 
   @Bean
   public InMemoryUserDetailsManager userDetailsService() {
-    UserDetails user = User.withUsername("omar1").password("{noop}omar1").roles("USER").build();
-    return new InMemoryUserDetailsManager(user);
+    UserDetails user1 = User.withUsername("omar1")
+        .password("{noop}omar1")
+        .roles("USER")
+        .build();
+    UserDetails user2 = User.withUsername("omar2")
+        .password(passwordEncoder().encode("omar2"))
+        .roles("USER")
+        .build();
+    UserDetails admin = User.withUsername("admin")
+        .password(passwordEncoder().encode("admin"))
+        .roles("ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(user1, user2, admin);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
 
