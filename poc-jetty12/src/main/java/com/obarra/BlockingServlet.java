140c96355a73ace4eedacb71f65237edd49850fc
obarra-dev <obarra@sonatype.com>
2025-11-18 12:16:27 -0300

    refactor BlockingServlet and Main2 for improved readability and structure

--- poc-jetty12/src/main/java/com/obarra/BlockingServlet.java
+++ poc-jetty12/src/main/java/com/obarra/BlockingServlet.java
@@ -2,18 +2,16 @@ package com.obarra;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class BlockingServlet extends HttpServlet
 {
-
   protected void doGet(
       HttpServletRequest request,
       HttpServletResponse response)
-      throws ServletException, IOException
+      throws IOException
   {
 
     response.setContentType("application/json");
