a197318db3b618302f1bebeb831bde925639e9ec
obarra-dev <obarra@sonatype.com>
2025-11-22 14:24:41 -0300

    test

--- poc-spring-batch/src/test/java/com/obarra/poc_spring_batch/PocSpringBatchApplicationTests.java
+++ poc-spring-batch/src/test/java/com/obarra/poc_spring_batch/PocSpringBatchApplicationTests.java
@@ -1,13 +1,26 @@
 package com.obarra.poc_spring_batch;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.job.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBatchTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = BatchConfig.class)
 class PocSpringBatchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private JobLauncherTestUtils jobLauncherTestUtils;
 
+  @Test
+  void contextLoads() throws Exception {
+    JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+    Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+  }
 }
